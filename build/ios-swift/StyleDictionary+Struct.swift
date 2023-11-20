cf3f163da34270d7108aa34d46876fc589f2d186
Leo <leonardocs@comfortdelgro.com>
2023-11-20 22:37:24 +0800

    Generate template structure

---
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -0,0 +1,55 @@
+
+//
+// StyleDictionary+Struct.swift
+//
+
+// Do not edit directly
+// Generated on Mon, 20 Nov 2023 14:33:24 GMT
+
+
+import UIKit
+
+internal struct StyleDictionaryStruct {
+    internal static let colorBaseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 1)
+    internal static let colorBaseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    internal static let colorBaseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
+    internal static let colorBaseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    internal static let colorBaseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    internal static let colorFontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
+    internal static let colorFontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
+    internal static let colorFontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    internal static let globalBackgroundBase = #FFFFFF
+    internal static let globalBackgroundLayer1 = #F7F8F9
+    internal static let globalBlueShade100 = #0142AF
+    internal static let globalBlueShades10 = #e6ecf7
+    internal static let globalBlueShades120 = #012158
+    internal static let globalBlueShades20 = #ccd9ef
+    internal static let globalBlueShades40 = #99b3df
+    internal static let globalBlueShades60 = #678ECF
+    internal static let globalBlueShades80 = #3468BF
+    internal static let globalGreyShades0 = #ffffff
+    internal static let globalGreyShades10 = #F7F8F9
+    internal static let globalGreyShades100 = #212121
+    internal static let globalGreyShades20 = #e6e6e6
+    internal static let globalGreyShades40 = #b4b4b4
+    internal static let globalGreyShades60 = #757575
+    internal static let globalGreyShades80 = #3e3e3e
+    internal static let globalOverlayShades80 = 21212173
+    internal static let globalSecondaryGreen50 = #4F9D43
+    internal static let globalSecondaryGreen90 = #e2f1df
+    internal static let globalSecondaryLinkBlue50 = #0094D9
+    internal static let globalSecondaryLinkBlue90 = #E3F6FD
+    internal static let globalSecondaryNeutral50 = #424D57
+    internal static let globalSecondaryNeutral90 = #E2E5E9
+    internal static let globalSecondaryOrange50 = #E57034
+    internal static let globalSecondaryOrange90 = #fceee7
+    internal static let globalSecondaryRed50 = #C44041
+    internal static let globalSecondaryRed90 = #FCE7E7
+    internal static let globalSecondaryYellow50 = #FFD14D
+    internal static let globalSecondaryYellow90 = #FFF4D1
+    internal static let globalStroke = #E2E5E9
+    internal static let sizeFontBase = CGFloat(16.00) /* the base size of the font */
+    internal static let sizeFontLarge = CGFloat(32.00) /* the large size of the font */
+    internal static let sizeFontMedium = CGFloat(16.00) /* the medium size of the font */
+    internal static let sizeFontSmall = CGFloat(12.00) /* the small size of the font */
+}
