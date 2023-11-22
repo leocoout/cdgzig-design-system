68bcf2e87aec31c93a56fe23d07e92b367e56a6f
Leo <leonardocs@comfortdelgro.com>
2023-11-22 23:35:33 +0800

    Update package and config file with correct token file

--- android/compose/StyleDictionaryColor.kt
+++ android/compose/StyleDictionaryColor.kt
@@ -1,7 +1,7 @@
 
 
 // Do not edit directly
-// Generated on Mon, 20 Nov 2023 16:41:35 GMT
+// Generated on Wed, 22 Nov 2023 15:34:24 GMT
 
 
 
@@ -11,12 +11,34 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.*
 
 object StyleDictionaryColor {
-  val colorBaseGrayDark = Color(0xff111111)
-  val colorBaseGrayLight = Color(0xffcccccc)
-  val colorBaseGrayMedium = Color(0xff999999)
-  val colorBaseGreen = Color(0xff00ff00)
-  val colorBaseRed = Color(0xffff0000)
-  val colorFontBase = Color(0xffff0000)
-  val colorFontSecondary = Color(0xff00ff00)
-  val colorFontTertiary = Color(0xffcccccc)
+  val colorBackgroundBase = Color(0xffffffff)
+  val colorBackgroundLayer1 = Color(0xfff7f8f9)
+  val colorBlueShade100 = Color(0xff0142af)
+  val colorBlueShades10 = Color(0xffe6ecf7)
+  val colorBlueShades120 = Color(0xff012158)
+  val colorBlueShades20 = Color(0xffccd9ef)
+  val colorBlueShades40 = Color(0xff99b3df)
+  val colorBlueShades60 = Color(0xff678ecf)
+  val colorBlueShades80 = Color(0xff3468bf)
+  val colorGreyShades0 = Color(0xffffffff)
+  val colorGreyShades10 = Color(0xfff7f8f9)
+  val colorGreyShades100 = Color(0xff212121)
+  val colorGreyShades20 = Color(0xffe6e6e6)
+  val colorGreyShades40 = Color(0xffb4b4b4)
+  val colorGreyShades60 = Color(0xff757575)
+  val colorGreyShades80 = Color(0xff3e3e3e)
+  val colorOverlayShades80 = Color(0x73212121)
+  val colorSecondaryGreen50 = Color(0xff4f9d43)
+  val colorSecondaryGreen90 = Color(0xffe2f1df)
+  val colorSecondaryLinkBlue50 = Color(0xff0094d9)
+  val colorSecondaryLinkBlue90 = Color(0xffe3f6fd)
+  val colorSecondaryNeutral50 = Color(0xff424d57)
+  val colorSecondaryNeutral90 = Color(0xffe2e5e9)
+  val colorSecondaryOrange50 = Color(0xffe57034)
+  val colorSecondaryOrange90 = Color(0xfffceee7)
+  val colorSecondaryRed50 = Color(0xffc44041)
+  val colorSecondaryRed90 = Color(0xfffce7e7)
+  val colorSecondaryYellow50 = Color(0xffffd14d)
+  val colorSecondaryYellow90 = Color(0xfffff4d1)
+  val colorStroke = Color(0xffe2e5e9)
 }
--- ios/DesignSystem/StyleDictionaryColor.swift
+++ ios/DesignSystem/StyleDictionaryColor.swift
@@ -4,18 +4,40 @@
 //
 
 // Do not edit directly
-// Generated on Mon, 20 Nov 2023 16:41:35 GMT
+// Generated on Wed, 22 Nov 2023 15:34:24 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryColor {
-    public static let baseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 1)
-    public static let baseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
-    public static let baseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
-    public static let baseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
-    public static let baseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
-    public static let fontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
-    public static let fontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
-    public static let fontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
+    public static let backgroundBase = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let backgroundLayer1 = UIColor(red: 0.969, green: 0.973, blue: 0.976, alpha: 1)
+    public static let blueShade100 = UIColor(red: 0.004, green: 0.259, blue: 0.686, alpha: 1)
+    public static let blueShades10 = UIColor(red: 0.902, green: 0.925, blue: 0.969, alpha: 1)
+    public static let blueShades120 = UIColor(red: 0.004, green: 0.129, blue: 0.345, alpha: 1)
+    public static let blueShades20 = UIColor(red: 0.800, green: 0.851, blue: 0.937, alpha: 1)
+    public static let blueShades40 = UIColor(red: 0.600, green: 0.702, blue: 0.875, alpha: 1)
+    public static let blueShades60 = UIColor(red: 0.404, green: 0.557, blue: 0.812, alpha: 1)
+    public static let blueShades80 = UIColor(red: 0.204, green: 0.408, blue: 0.749, alpha: 1)
+    public static let greyShades0 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let greyShades10 = UIColor(red: 0.969, green: 0.973, blue: 0.976, alpha: 1)
+    public static let greyShades100 = UIColor(red: 0.129, green: 0.129, blue: 0.129, alpha: 1)
+    public static let greyShades20 = UIColor(red: 0.902, green: 0.902, blue: 0.902, alpha: 1)
+    public static let greyShades40 = UIColor(red: 0.706, green: 0.706, blue: 0.706, alpha: 1)
+    public static let greyShades60 = UIColor(red: 0.459, green: 0.459, blue: 0.459, alpha: 1)
+    public static let greyShades80 = UIColor(red: 0.243, green: 0.243, blue: 0.243, alpha: 1)
+    public static let overlayShades80 = UIColor(red: 0.129, green: 0.129, blue: 0.129, alpha: 0.45098039215686275)
+    public static let secondaryGreen50 = UIColor(red: 0.310, green: 0.616, blue: 0.263, alpha: 1)
+    public static let secondaryGreen90 = UIColor(red: 0.886, green: 0.945, blue: 0.875, alpha: 1)
+    public static let secondaryLinkBlue50 = UIColor(red: 0.000, green: 0.580, blue: 0.851, alpha: 1)
+    public static let secondaryLinkBlue90 = UIColor(red: 0.890, green: 0.965, blue: 0.992, alpha: 1)
+    public static let secondaryNeutral50 = UIColor(red: 0.259, green: 0.302, blue: 0.341, alpha: 1)
+    public static let secondaryNeutral90 = UIColor(red: 0.886, green: 0.898, blue: 0.914, alpha: 1)
+    public static let secondaryOrange50 = UIColor(red: 0.898, green: 0.439, blue: 0.204, alpha: 1)
+    public static let secondaryOrange90 = UIColor(red: 0.988, green: 0.933, blue: 0.906, alpha: 1)
+    public static let secondaryRed50 = UIColor(red: 0.769, green: 0.251, blue: 0.255, alpha: 1)
+    public static let secondaryRed90 = UIColor(red: 0.988, green: 0.906, blue: 0.906, alpha: 1)
+    public static let secondaryYellow50 = UIColor(red: 1.000, green: 0.820, blue: 0.302, alpha: 1)
+    public static let secondaryYellow90 = UIColor(red: 1.000, green: 0.957, blue: 0.820, alpha: 1)
+    public static let stroke = UIColor(red: 0.886, green: 0.898, blue: 0.914, alpha: 1)
 }
